d0aedc6a6341170a99f67be22ae452b206c7173a
ghena-droid <ghina9552@gmail.com>
2026-01-06 01:00:29 +0400

    Initial version of duplicate removal algorithm (main branch)

---
+++ duplicate_sales_cleaner.py
@@ -0,0 +1,21 @@
+sales_records = ["A001", "A002", "A001", "A003", "A002", "A004"]
+
+unique_sales = []
+
+for record in sales_records:
+
+    is_duplicate = False
+   
+    for item in unique_sales:
+        
+        if record == item:
+            
+            is_duplicate = True
+            
+            break
+    
+    if is_duplicate == False:
+        
+        unique_sales.append(record)
+
+print("Unique Sales Records:", unique_sales)
